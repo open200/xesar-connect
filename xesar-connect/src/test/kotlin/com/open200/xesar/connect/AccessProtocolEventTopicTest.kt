76855efbe92bb972995aeb5e7dd75703dda61e7d
katharina.bernal <147484433+berka3@users.noreply.github.com>
2024-02-19 15:21:56 +0100

    feat: add access protocol event convenience methods

---
+++ xesar-connect/src/test/kotlin/com/open200/xesar/connect/AccessProtocolEventTopicTest.kt
@@ -0,0 +1,22 @@
+package com.open200.xesar.connect
+
+import com.open200.xesar.connect.messages.query.EventType
+import com.open200.xesar.connect.messages.query.GroupOfEvent
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import java.util.*
+
+class AccessProtocolEventTopicTest :
+    FunSpec({
+        test("accessProtocolEventTopic should return correct topic for NORMAL_OPENING event") {
+            Topics.Event.accessProtocolEventTopic(
+                    GroupOfEvent.MediumEvents, EventType.NORMAL_OPENING)
+                .shouldBe("xs3/1/ase/MediumEvents/0001")
+        }
+
+        test(
+            "accessProtocolEventTopic should return correct topic for every access protocol event") {
+                Topics.Event.accessProtocolEventTopic(GroupOfEvent.MediumEvents)
+                    .shouldBe("xs3/1/ase/MediumEvents/+")
+            }
+    })
