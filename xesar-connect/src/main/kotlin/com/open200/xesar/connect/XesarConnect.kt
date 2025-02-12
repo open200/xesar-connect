075f84949bce426fd2ba279d7ceae435a173469b
katharina.bernal <147484433+berka3@users.noreply.github.com>
2025-02-12 12:21:10 +0100

    fix: change timeout event logging level from error to debug regardless of event requirement

--- xesar-connect/src/main/kotlin/com/open200/xesar/connect/XesarConnect.kt
+++ xesar-connect/src/main/kotlin/com/open200/xesar/connect/XesarConnect.kt
@@ -691,11 +691,7 @@ class XesarConnect(private val client: IXesarMqttClient, val config: Config) {
             try {
                 withTimeout(requestConfig.timeout) { eventDeferred.await() }
             } catch (e: TimeoutCancellationException) {
-                val logMessage = "timeout while waiting for event $topicEvent"
-                when (eventRequired) {
-                    true -> logger.error { logMessage }
-                    false -> logger.debug { logMessage }
-                }
+                logger.debug { "timeout while waiting for event $topicEvent" }
                 completeWithSpecificException(
                     getTypeOfExceptionDependingOnEventRequired(eventRequired, topicEvent, e),
                     listOf(eventDeferred))
