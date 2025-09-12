a3995d985fe6cf41d6d9f98babf95a7ab3f4f9ab
paulwinter <paul.winter@open200.com>
2025-09-12 12:58:44 +0200

    feature: add additional values to PartitionChanged event

--- xesar-connect/src/main/kotlin/com/open200/xesar/connect/messages/event/PartitionChanged.kt
+++ xesar-connect/src/main/kotlin/com/open200/xesar/connect/messages/event/PartitionChanged.kt
@@ -1,5 +1,7 @@
 package com.open200.xesar.connect.messages.event
 
+import com.open200.xesar.connect.messages.EntityMetadata
+import com.open200.xesar.connect.messages.MobileServiceMode
 import com.open200.xesar.connect.messages.PersonalLog
 import com.open200.xesar.connect.utils.LocalTimeSerializer
 import com.open200.xesar.connect.utils.UUIDSerializer
@@ -21,7 +23,12 @@ import kotlinx.serialization.Serializable
  * @param id The ID of the partition.
  * @param dailySchedulerExecutionTime The daily scheduler execution time.
  * @param smartphoneValidityDuration The default smartphone validity duration for all smartphones of
- *   this partition
+ *   this partition.
+ * @param smartphoneValidityThreshold The smartphone validity threshold.
+ * @param entityMetadataDefinitions Contains the information for all defined custom data fields.
+ * @param maximumEntityMetadataDefinitions The maximum allowed custom data fields per entity type.
+ * @param mobileServiceMode The Mobile Service mode. Used to describe the application's method of
+ *   communication with Smartphones.
  */
 @Serializable
 data class PartitionChanged(
@@ -37,4 +44,31 @@ data class PartitionChanged(
     val dailySchedulerExecutionTime: @Serializable(with = LocalTimeSerializer::class) LocalTime? =
         null,
     val smartphoneValidityDuration: Short? = null,
-) : Event
+    val smartphoneValidityThreshold: Short? = null,
+    val entityMetadataDefinitions: EntityMetadataDefinitions? = null,
+    val maximumEntityMetadataDefinitions: Short? = null,
+    val mobileServiceMode: MobileServiceMode? = null,
+) : Event {
+
+    /**
+     * Represents the container for all entity types and their respective defined custom data
+     * fields.
+     *
+     * @param authorizationProfiles The custom metadata that can be set for each authorization
+     *   profile entity.
+     * @param identificationMedia The custom metadata that can be set for each identification medium
+     *   entity.
+     * @param installationPoints The custom metadata that can be set for each installation point
+     *   entity.
+     * @param persons The custom metadata that can be set for each person entity.
+     * @param zones The custom metadata that can be set for each zone entity.
+     */
+    @Serializable
+    data class EntityMetadataDefinitions(
+        val authorizationProfiles: List<EntityMetadata>,
+        val identificationMedia: List<EntityMetadata>,
+        val installationPoints: List<EntityMetadata>,
+        val persons: List<EntityMetadata>,
+        val zones: List<EntityMetadata>,
+    )
+}
