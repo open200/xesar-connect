eb9586411dcc4107e588d6b60fa115e7c408cc7d
paulwinter <paul.winter@open200.com>
2025-09-25 14:42:15 +0200

    BREAKING CHANGE: add entityMetadata to createPerson command and method. change changePersonInformation, deletePerson, setDefaultAuthorizationProfileForPerson, setDefaultDisengagePeriodForPerson and setPersonalReferenceDurationInPerson command and methods to reflect the new addition of the internal person id as possible identifier.

--- xesar-connect/src/main/kotlin/com/open200/xesar/connect/messages/command/DeletePersonMapi.kt
+++ xesar-connect/src/main/kotlin/com/open200/xesar/connect/messages/command/DeletePersonMapi.kt
@@ -9,11 +9,13 @@ import kotlinx.serialization.Serializable
  *
  * @param commandId The id of the command.
  * @param externalId The external id of the person to delete.
+ * @param id The id of the person.
  * @param token The token of the command.
  */
 @Serializable
 data class DeletePersonMapi(
     override val commandId: @Serializable(with = UUIDSerializer::class) UUID,
     val externalId: String? = null,
+    val id: @Serializable(with = UUIDSerializer::class) UUID? = null,
     val token: String,
 ) : Command
