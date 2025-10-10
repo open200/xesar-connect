e369c7e30759e9778edfdac8a8cd19292d6ccf1f
paulwinter <paul.winter@open200.com>
2025-10-10 13:03:34 +0200

    test: add an actual list of entityMetadata to the fixture and query tests (#164)

--- xesar-connect/src/test/kotlin/com/open200/xesar/connect/util/fixture/InstallationPointFixture.kt
+++ xesar-connect/src/test/kotlin/com/open200/xesar/connect/util/fixture/InstallationPointFixture.kt
@@ -1,6 +1,7 @@
 package com.open200.xesar.connect.util.fixture
 
 import com.open200.xesar.connect.messages.ComponentType
+import com.open200.xesar.connect.messages.EntityMetadata
 import com.open200.xesar.connect.messages.query.InstallationPoint
 import com.open200.xesar.connect.messages.query.OnlineStatus
 import java.util.*
@@ -28,6 +29,18 @@ object InstallationPointFixture {
             shopMode = false,
             openDoor = true,
             bleStatus = "NO_BLE",
-            entityMetadata = emptyList(),
+            entityMetadata =
+                listOf(
+                    EntityMetadata(
+                        id = UUID.fromString("123e4567-e89b-12d3-a456-426614174000"),
+                        name = "type",
+                        value = "double door",
+                    ),
+                    EntityMetadata(
+                        id = UUID.fromString("0f8fad5b-d9cb-469f-a165-70867728950e"),
+                        name = "color",
+                        value = null,
+                    ),
+                ),
         )
 }
