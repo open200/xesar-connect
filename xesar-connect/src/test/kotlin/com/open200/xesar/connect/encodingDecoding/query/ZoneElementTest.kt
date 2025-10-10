a0c620c5db665426869e3270132b155cabea0165
paulwinter <paul.winter@open200.com>
2025-10-10 12:54:32 +0200

    test: add an actual list of entityMetadata to the fixture and query tests (#163)

--- xesar-connect/src/test/kotlin/com/open200/xesar/connect/encodingDecoding/query/ZoneElementTest.kt
+++ xesar-connect/src/test/kotlin/com/open200/xesar/connect/encodingDecoding/query/ZoneElementTest.kt
@@ -16,7 +16,7 @@ class ZoneElementTest :
             )
 
         val zoneString =
-            "{\"requestId\":\"d385ab22-0f51-4b97-9ecd-b8ff3fd4fcb6\",\"response\":{\"installationPoints\":[\"7ca59670-bd30-4ea9-9bd1-2103a9bd2f2a\"],\"partitionId\":\"7b4399a0-21ce-4bee-ba43-e06e291248d2\",\"installationPointCount\":0,\"name\":\"zone name\",\"description\":\"zone description\",\"id\":\"497f6eca-6276-4993-bfeb-53cbbbba6f08\",\"entityMetadata\":[]}}"
+            "{\"requestId\":\"d385ab22-0f51-4b97-9ecd-b8ff3fd4fcb6\",\"response\":{\"installationPoints\":[\"7ca59670-bd30-4ea9-9bd1-2103a9bd2f2a\"],\"partitionId\":\"7b4399a0-21ce-4bee-ba43-e06e291248d2\",\"installationPointCount\":0,\"name\":\"zone name\",\"description\":\"zone description\",\"id\":\"497f6eca-6276-4993-bfeb-53cbbbba6f08\",\"entityMetadata\":[{\"id\":\"123e4567-e89b-12d3-a456-426614174000\",\"name\":\"type\",\"value\":\"floor\"},{\"id\":\"0f8fad5b-d9cb-469f-a165-70867728950e\",\"name\":\"city\",\"value\":null}]}}"
 
         test("encoding QueryElement for a zone") {
             val zoneEncoded = encodeQueryElement(zone)
--- xesar-connect/src/test/kotlin/com/open200/xesar/connect/util/fixture/ZoneFixture.kt
+++ xesar-connect/src/test/kotlin/com/open200/xesar/connect/util/fixture/ZoneFixture.kt
@@ -1,5 +1,6 @@
 package com.open200.xesar.connect.util.fixture
 
+import com.open200.xesar.connect.messages.EntityMetadata
 import com.open200.xesar.connect.messages.query.Zone
 import java.util.*
 
@@ -13,6 +14,18 @@ object ZoneFixture {
             description = "zone description",
             id = UUID.fromString("497f6eca-6276-4993-bfeb-53cbbbba6f08"),
             installationPoints = listOf(UUID.fromString("7ca59670-bd30-4ea9-9bd1-2103a9bd2f2a")),
-            entityMetadata = emptyList(),
+            entityMetadata =
+                listOf(
+                    EntityMetadata(
+                        id = UUID.fromString("123e4567-e89b-12d3-a456-426614174000"),
+                        name = "type",
+                        value = "floor",
+                    ),
+                    EntityMetadata(
+                        id = UUID.fromString("0f8fad5b-d9cb-469f-a165-70867728950e"),
+                        name = "city",
+                        value = null,
+                    ),
+                ),
         )
 }
